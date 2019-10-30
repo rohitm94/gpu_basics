02a7b8bbc56959b99b83c259b923575bb57cb788
rohitm94 <rohit11094@gmail.com>
2019-10-29 23:43:29 -0400

    gpu final check polynomial code

--- polynomialexpansion_GPU_code.cu
+++ polynomialexpansion_GPU_code.cu
@@ -43,41 +43,42 @@ int main(int argc, char *argv[])
     for (int i = 0; i < degree + 1; ++i)
         poly[i] = 1.;
 
-    float *dev_array, *dev_poly;
+    float *d_array, *d_poly;
 
-    cudaMalloc((void **)&dev_array, n * sizeof(float));
-    cudaMalloc((void **)&dev_poly, (degree + 1) * sizeof(float));
+    cudaMalloc((void **)&d_array, n * sizeof(float));
+    cudaMalloc((void **)&d_poly, (degree + 1) * sizeof(float));
 
-    cudaMemcpy(dev_array, array, n * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_poly, poly, (degree + 1) * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_array, array, n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_poly, poly, (degree + 1) * sizeof(float), cudaMemcpyHostToDevice);
 
     std::chrono::time_point<std::chrono::system_clock> begin, end;
     begin = std::chrono::system_clock::now();
 
     for (int iter = 0; iter < nbiter; ++iter)
-        polynomial_expansion<<<(n + BLOCKSIZE - 1) / BLOCKSIZE, BLOCKSIZE>>>(dev_poly, degree, n, dev_array);
+        polynomial_expansion<<<(n + BLOCKSIZE - 1) / BLOCKSIZE, BLOCKSIZE>>>(d_poly, degree, n, d_array);
 
     cudaDeviceSynchronize();
-    cudaMemcpy(array, dev_array, n * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(array, d_array, n * sizeof(float), cudaMemcpyDeviceToHost);
 
-    cudaFree(dev_array);
-    cudaFree(dev_poly);
+    cudaFree(d_array);
+    cudaFree(d_poly);
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> totaltime = (end - begin) / nbiter;
 
     {
-        bool correct = true;
-        int ind;
+        bool check = true;
+        int index;
         for (int i = 0; i < n; ++i)
         {
             if (fabs(array[i] - (degree + 1)) > 0.01)
             {
-                correct = false;
-                ind = i;
+                check = false;
+                index = i;
+				break;
             }
         }
-        if (!correct)
-            std::cerr << "Result is incorrect. In particular array[" << ind << "] should be " << degree + 1 << " not " << array[ind] << std::endl;
+        if (!check)
+            std::cerr << "Polynomial expansion has corrupted at array[" << index << "]" << std::endl;
     }
 
     std::cerr << array[0] << std::endl;
